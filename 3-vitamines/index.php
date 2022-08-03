3856a231335d57febdea214c2b5e4912bc0fbe5a
clem0802 <hhcc0802@gmail.com>
2022-08-03 22:27:48 +0200

    --

--- 3-vitamines/index.php
+++ 3-vitamines/index.php
@@ -4,15 +4,25 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>EXO (Lawrence) - Fruggies (clémence)</title>
+        <!-- bootstrap -->
+        <link 
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" 
+            rel="stylesheet" 
+            integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" 
+            crossorigin="anonymous"
+        /> 
+        <link rel="stylesheet" href="style.css">
+        <title>EXO2 (Lawrence) - Fruggies (clémence)</title>
+        <meta name="description" content="EXO2 (Lawrence) - Fruggies (clémence)">
     </head>
 
-    <body>
-        <h1>EXO (Lawrence) <br> Fruggies (clémence)</h1>
+    <body class="container-fluid d-flex flex-column justify-content-center align-items-center text-center">
+        <h1>EXO2 (Lawrence's lesson)</h1><br>
+        <h2>Fruggies (clémence)</h2>
 
         <?php
         // Connexion à la base de données (paramètres de connexion)
-        // (MY path) http://localhost/y.doranco-5-PHP/3-fruggie/index.php
+        // (MY path) http://localhost/y.doranco-5-PHP/3-vitamines/index.php
         $adresseBdd = "localhost:3306"; // Mac: 8889, Windows:3306 (moi)
         $utilisateurBdd = "root";
         $mdpUtilisateurBdd = "root"; // Mac: "root", Windows: ""
@@ -32,20 +42,18 @@
         }
         ?>
 
-
-        <hr><br>
-
         <!-- FORM -->
-        <form method="POST">
+        <form method="POST" class="container-fluid d-flex flex-column">
             <h2>Fruggie Subscription Form</h2>
             <p>Which one would you prefer? fruit or veggie?</p>
-            <p>Please subscribe first and we will see.</p>
+            <p>Please subscribe first to find out.</p>
             <input type="text" name="prenom" placeholder="Votre prénom">
             <input type="text" name="email" placeholder="Votre email">
             <input type="password" name="mdp" placeholder="Votre mot de passe">
             <input type="password" name="mdp-confirmation" placeholder="Confirmez le mot de passe">
-            <input type="submit" name="inscription" value="S'inscrire">
+            <input type="submit" name="inscription" value="S'inscrire" class="submit">
         </form>
+        <br>
 
         <!-- on va vérifier si le formulaire est soumis -->
         <?php 
@@ -77,24 +85,26 @@
 
 
 
-        <!-- ******************************************************** -->
-        <!-- pour récupérer les deux articles qu'on a créés -->
-        <!-- exécuter une requête SQL, pour les afficher sur le browser -->
-        <!-- faire un boucle pour parcourir chaque article -->
-        <?php
-            // $articles = $connexion->query("SELECT * FROM articles WHERE id = 1 OR id = 2");
-            $fruggies = $connexion->query("SELECT * FROM fruggies");
+        <main class="container-fluid d-flex flex-column justify-content-center align-items-center">
+            <!-- ******************************************************** -->
+            <!-- pour récupérer les deux articles qu'on a créés -->
+            <!-- exécuter une requête SQL, pour les afficher sur le browser -->
+            <!-- faire un boucle pour parcourir chaque article -->
+            <?php
+                // $articles = $connexion->query("SELECT * FROM articles WHERE id = 1 OR id = 2");
+                $fruggies = $connexion->query("SELECT * FROM fruggies");
 
-            echo "<ul>";
-            foreach($fruggies as $fruggie) {
-                echo "<li>
-                <h3>" . $fruggie['name'] . "</h3>
-                <p>Fruggie category: " . $fruggie['category'] . "</p>
-                <p>Fruggie detail: " . $fruggie['description'] . "</p>
-                     </li>";
-            }
-            echo "</ul>";
-        ?>
+                echo "<ul>";
+                foreach($fruggies as $fruggie) {
+                    echo "<li>
+                    <h3>" . $fruggie['name'] . "</h3>
+                    <p>Fruggie category: " . $fruggie['category'] . "</p>
+                    <p>Fruggie detail: " . $fruggie['description'] . "</p>
+                        </li>";
+                }
+                echo "</ul>";
+            ?>
+        </main>
        
     </body>
 </html>
